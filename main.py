659b514be925d53a919a63690ab57b92c2651377
christian03-web <christian.03@hotmail.it>
2026-02-17 13:52:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,9 @@ def index():
     return render_template("index.html", matches=matches)
 
 import os
+
 application = app
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
